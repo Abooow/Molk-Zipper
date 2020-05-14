1362acbe38016d8ed7a5c4f4bf507d96d170577b
Ryuu <evil__dark__angel@hotmail.com>
2020-05-14 16:42:15 +0200

    dsa
    dsadd

---
+++ Molk-Zipper/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Molk_Zipper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //private Molk popupPuzzlePiecesWindow = null;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void click_on_molk(object sender, RoutedEventArgs e)
+        {
+            Molk_Btn.Visibility = Visibility.Collapsed;
+            Unmolk_Btn.Visibility = Visibility.Collapsed;
+            Lable_Molk.Visibility = Visibility.Collapsed;
+            Lable_UnMolk.Visibility = Visibility.Collapsed;
+
+        }
+    }
+}
---
+++ Molk-Zipper/Molk.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Molk_Zipper
+{
+    /// <summary>
+    /// Interaction logic for Molk.xaml
+    /// </summary>
+    public partial class Molk : Window
+    {
+        public Molk()
+        {
+            InitializeComponent();
+        }
+
+        private void Written_text_ContextMenuOpening(object sender, ContextMenuEventArgs e)
+        {
+
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+
+    }
+}
